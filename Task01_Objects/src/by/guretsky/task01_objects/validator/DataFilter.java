44897eb6015c5c6d80c4787affc0191f08579256
ilyaguretsky <borec023017@gmail.com>
2019-02-15 19:53:32 +0300

    Delete

--- Task01_Objects/src/by/guretsky/task01_objects/validator/DataFilter.java
+++ Task01_Objects/src/by/guretsky/task01_objects/validator/DataFilter.java
@@ -1,11 +1,8 @@
 package by.guretsky.task01_objects.validator;
 
-import by.guretsky.task01_objects.exception.FileDoesNotExistException;
-import by.guretsky.task01_objects.reader.DataReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +16,6 @@ public class DataFilter {
         final int amountOfNumbersInLine = 8;
         boolean isCorrect;
         for (String str : fileData) {
-            /*if (str.equals("")) {
-                LOGGER.info("Line is empty");
-            }*/
             isCorrect = true;
             String[] splitString = str.split(SPLIT_PATTERN);
             for (String elem : splitString) {
@@ -36,15 +30,5 @@ public class DataFilter {
         }
 
         return filteredData;
-    }
-
-    public static void main(String[] args) throws FileDoesNotExistException {
-        DataReader reader = new DataReader("data" + File.separator
-                + "info.txt");
-        DataFilter filter = new DataFilter();
-        List<String> list = new ArrayList<>();
-        list = reader.readStringList();
-        List<String> flist = filter.filterFileData(list);
-        System.out.println(flist);
     }
 }
