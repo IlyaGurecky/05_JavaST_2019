3c8c03c392ce94191a741983f35bf2baf881f924
ilyaguretsky <borec023017@gmail.com>
2019-02-20 02:25:19 +0300

    Add exceptions documentations

--- Task01_Objects/src/by/guretsky/task01_objects/exception/EmptyFileException.java
+++ Task01_Objects/src/by/guretsky/task01_objects/exception/EmptyFileException.java
@@ -1,9 +1,14 @@
 package by.guretsky.task01_objects.exception;
 
+/**
+ * Signals that file is empty.
+ */
 public class EmptyFileException extends Exception {
-    public EmptyFileException() {
-    }
-
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message detail message
+     */
     public EmptyFileException(final String message) {
         super(message);
     }
--- Task01_Objects/src/by/guretsky/task01_objects/exception/IncorrectQuadranglePointsException.java
+++ Task01_Objects/src/by/guretsky/task01_objects/exception/IncorrectQuadranglePointsException.java
@@ -1,6 +1,14 @@
 package by.guretsky.task01_objects.exception;
 
+/**
+ * Signals that points can't create a quadrangle.
+ */
 public class IncorrectQuadranglePointsException extends Exception {
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message detail message
+     */
     public IncorrectQuadranglePointsException(final String message) {
         super(message);
     }
