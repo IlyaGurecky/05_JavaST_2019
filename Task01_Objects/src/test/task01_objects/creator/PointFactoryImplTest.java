9c1600ac5f4b38d12b49bef5bb8d32548f72ce4c
ilyaguretsky <borec023017@gmail.com>
2019-02-19 17:21:08 +0300

    Added Factory Method tests and deleted creator test

--- Task01_Objects/src/test/task01_objects/creator/PointFactoryImplTest.java
+++ Task01_Objects/src/test/task01_objects/creator/PointFactoryImplTest.java
@@ -1,7 +1,7 @@
 package test.task01_objects.creator;
 
-import by.guretsky.task01_objects.creator.PointsListCreator;
 import by.guretsky.task01_objects.entity.Point;
+import by.guretsky.task01_objects.factory.PointFactoryImpl;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,19 +10,19 @@ import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("CheckStyle")
-public class PointsListCreatorTest {
+public class PointFactoryImplTest {
 
-    @Test(description = "Positive script for point creator")
+    @Test(description = "Positive script for point list creator")
     public void testCreatePoints() {
         List<Double> xy = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0,
                 6.0, 7.0, 8.0));
-        PointsListCreator creator = new PointsListCreator();
+        PointFactoryImpl creator = new PointFactoryImpl();
         List<Point> expected = new ArrayList<>(Arrays
                 .asList(new Point(1.0, 2.0),
                         new Point(3.0, 4.0),
                         new Point(5.0, 6.0),
                         new Point(7.0, 8.0)));
-        List<Point> actualPointsList = creator.createPoints(xy);
+        List<Point> actualPointsList = creator.createPointsList(xy);
 
         Assert.assertEquals(actualPointsList, expected);
     }
