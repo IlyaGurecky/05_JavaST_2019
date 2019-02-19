ed69cdf9044c79f565991664d3284602364ed2bc
ilyaguretsky <borec023017@gmail.com>
2019-02-19 17:18:18 +0300

    Deleted creators

--- Task01_Objects/src/by/guretsky/task01_objects/creator/PointCreator.java
+++
@@ -1,9 +0,0 @@
-package by.guretsky.task01_objects.creator;
-
-import by.guretsky.task01_objects.entity.Point;
-
-public class PointCreator {
-    public Point createPoint(final Double x, final Double y) {
-        return new Point(x, y);
-    }
-}
