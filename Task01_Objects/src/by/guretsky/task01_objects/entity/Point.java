530b383df6cafd05e5d4cc2768a8afe0abbeb579
ilyaguretsky <borec023017@gmail.com>
2019-02-20 02:02:49 +0300

    Add documentation in the entity classes

--- Task01_Objects/src/by/guretsky/task01_objects/entity/Point.java
+++ Task01_Objects/src/by/guretsky/task01_objects/entity/Point.java
@@ -1,34 +1,80 @@
 package by.guretsky.task01_objects.entity;
 
-
+/**
+ * This class used to store information about point.
+ *
+ * @author ilyaguretsky
+ */
 public class Point {
+    /**
+     * X coordinate.
+     */
     private Double x;
+    /**
+     * Y coordinate.
+     */
     private Double y;
 
+    /**
+     * Private constructor without parameters.
+     *
+     * @see Point#Point(Double, Double)
+     */
     private Point() {
     }
 
+    /**
+     * Constructor - to create new object with special parameters.
+     *
+     * @param pointX X coordinate of the Point
+     * @param pointY Y coordinate of the Point
+     */
     public Point(final Double pointX, final Double pointY) {
         x = pointX;
         y = pointY;
     }
 
+    /**
+     * Function get the value {@link Point#x}.
+     *
+     * @return X coordinate
+     */
     public Double getX() {
         return x;
     }
 
+    /**
+     * Set value of X coordinate, which you can get used the method
+     * {@link Point#getX()}.
+     *
+     * @param pointX new value of the X coordinate
+     */
     public void setX(final Double pointX) {
         this.x = pointX;
     }
 
+    /**
+     * Function get the value {@link Point#y}.
+     *
+     * @return Y coordinate
+     */
     public Double getY() {
         return y;
     }
 
+    /**
+     * Set value of Y coordinate, which you can get used the method
+     * {@link Point#getY()}.
+     *
+     * @param pointY new value of the Y coordinate
+     */
     public void setY(final Double pointY) {
         this.y = pointY;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -42,6 +88,9 @@ public class Point {
                 && y.equals(point.y);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -51,6 +100,9 @@ public class Point {
         return result;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "Point{" + "x=" + x + ", y=" + y + '}';
--- Task01_Objects/src/by/guretsky/task01_objects/entity/Quadrangle.java
+++ Task01_Objects/src/by/guretsky/task01_objects/entity/Quadrangle.java
@@ -2,28 +2,67 @@ package by.guretsky.task01_objects.entity;
 
 import java.util.List;
 
+/**
+ * This class used to store information about quadrangle.
+ *
+ * @author ilyaguretsky
+ */
 public class Quadrangle {
+    /**
+     * List of the {@link Point}, which form a quadrangle.
+     */
     private List<Point> points;
 
+    /**
+     * Private constructor without parameters.
+     *
+     * @see Quadrangle#Quadrangle(List)
+     */
     private Quadrangle() {
     }
 
+    /**
+     * Constructor - to create new object with special parameter.
+     *
+     * @param pointsFromFile list of the {@link Point}
+     */
     public Quadrangle(final List<Point> pointsFromFile) {
         this.points = pointsFromFile;
     }
 
+    /**
+     * Function get the value {@link Point} from the {@link Quadrangle#points}.
+     *
+     * @param index number of the point
+     * @return one {@link Point} according to the index
+     */
     public Point getPoint(final int index) {
         return points.get(index);
     }
 
+    /**
+     * Set X coordinate of the {@link Point} from {@link Quadrangle#points}.
+     *
+     * @param pointIndex index of the necessary point
+     * @param x          new value of the X coordinate
+     */
     public void setPointX(final int pointIndex, final Double x) {
         this.points.get(pointIndex).setX(x);
     }
 
+    /**
+     * Set Y coordinate of the {@link Point} from {@link Quadrangle#points}.
+     *
+     * @param pointIndex index of the necessary point
+     * @param y          new value of the X coordinate
+     */
     public void setPointY(final int pointIndex, final Double y) {
         this.points.get(pointIndex).setY(y);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -36,6 +75,9 @@ public class Quadrangle {
         return points.equals(that.points);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
