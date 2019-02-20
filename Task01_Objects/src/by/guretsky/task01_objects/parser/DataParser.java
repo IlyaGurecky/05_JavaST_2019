1f659b8eaf9c0e70f56f25548c21a96b20608870
ilyaguretsky <borec023017@gmail.com>
2019-02-20 11:01:38 +0300

    Add parser documentations

--- Task01_Objects/src/by/guretsky/task01_objects/parser/DataParser.java
+++ Task01_Objects/src/by/guretsky/task01_objects/parser/DataParser.java
@@ -5,9 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class include method, which parse string numbers into double numbers.
+ */
 public class DataParser {
+    /**
+     * Regular expression for split string.
+     */
     private static final String SPLIT_PATTERN = "\\s+";
 
+    /**
+     * Method parse list of the string numbers into double list and add this
+     * list in Map.
+     *
+     * @param filteredList list of the string numbers, which we want to parse
+     *                     to double
+     * @return Map, which include the lists of the double numbers.
+     */
     public Map<Integer, List<Double>> parseDouble(final List<String>
                                                           filteredList) {
         Map<Integer, List<Double>> parsedDataMap = new HashMap<>();
