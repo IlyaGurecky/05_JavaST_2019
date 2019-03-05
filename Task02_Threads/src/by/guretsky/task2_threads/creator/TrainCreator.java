fa2ecd59036dee70607dff96c5365f458818b962
ilyaguretsky <borec023017@gmail.com>
2019-03-06 00:40:26 +0300

    Update Creator

--- Task02_Threads/src/by/guretsky/task2_threads/creator/TrainCreator.java
+++ Task02_Threads/src/by/guretsky/task2_threads/creator/TrainCreator.java
@@ -2,17 +2,24 @@ package by.guretsky.task2_threads.creator;
 
 import by.guretsky.task2_threads.entity.Train;
 import by.guretsky.task2_threads.validator.TrainValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TrainCreator {
+    private static final Logger LOGGER
+            = LogManager.getLogger(TrainCreator.class);
+
     public List<Train> createTrainsList(final List<Integer> directions) {
         List<Train> trains = new ArrayList<>();
         TrainValidator validator = new TrainValidator();
         directions.forEach(direction -> {
             if (validator.validateInfo(direction)) {
                 trains.add(new Train(direction));
+            } else {
+                LOGGER.info("Direction: " + direction + " is incorrect");
             }
         });
         return trains;
