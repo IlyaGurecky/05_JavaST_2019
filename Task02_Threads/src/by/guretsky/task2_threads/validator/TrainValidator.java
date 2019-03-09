7f512229b592d81fed3a0b6930b8266558672a07
ilyaguretsky <borec023017@gmail.com>
2019-03-10 01:22:24 +0300

    Add documentation

--- Task02_Threads/src/by/guretsky/task2_threads/validator/TrainValidator.java
+++ Task02_Threads/src/by/guretsky/task2_threads/validator/TrainValidator.java
@@ -4,7 +4,13 @@ package by.guretsky.task2_threads.validator;
  * Validation class. Check if data is correct to create an object.
  */
 public class TrainValidator {
-
+    /**
+     * The method checks if train direction is correct to create an
+     * {@link by.guretsky.task2_threads.entity.Train}.
+     *
+     * @param trainInfo train direction
+     * @return true if direction is correct
+     */
     public boolean validateInfo(final Integer trainInfo) {
         final int firstDirection = 1;
         final int secondDirection = 2;
