f30ce003b6513ed6f5735a94e5d0318042c128c0
ilyaguretsky <borec023017@gmail.com>
2019-03-10 01:27:38 +0300

    Add documentation

--- Task02_Threads/src/by/guretsky/task2_threads/creator/TrainCreator.java
+++ Task02_Threads/src/by/guretsky/task2_threads/creator/TrainCreator.java
@@ -9,10 +9,22 @@ import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Train creator class, which include method to create {@link Train} object.
+ */
 public class TrainCreator {
+    /**
+     * Logger, which used to log events.
+     */
     private static final Logger LOGGER
             = LogManager.getLogger(TrainCreator.class);
 
+    /**
+     * The method create list of the {@link Train} objects.
+     *
+     * @param directions list of the trains directions.
+     * @return train list
+     */
     public List<Train> createTrainsList(final List<Integer> directions) {
         List<Train> trains = new ArrayList<>();
         TrainValidator validator = new TrainValidator();
