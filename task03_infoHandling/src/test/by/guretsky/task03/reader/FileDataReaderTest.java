58a9b8b3717d1b0b1add45147bc5f30ceb0ef84c
ilyaguretsky <borec023017@gmail.com>
2019-03-15 23:01:24 +0300

    add reader test

---
+++ task03_infoHandling/src/test/by/guretsky/task03/reader/FileDataReaderTest.java
@@ -0,0 +1,34 @@
+package test.by.guretsky.task03.reader;
+
+import by.guretsky.task03.exception.FileException;
+import by.guretsky.task03.reader.FileDataReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class FileDataReaderTest {
+    private static final String NULL_FILE_PATH = null;
+    private static final String FILE_PATH = "data" + File.separator
+            + "test.txt";
+    private FileDataReader reader;
+
+    @Test(description = "Positive script for file data reader method")
+    public void testReadStringList() throws FileException {
+        reader = new FileDataReader(FILE_PATH);
+        String expected = "    It is a (111^5|1&2«(2|5»2&71))|1200 established"
+                + " fact that a reader will be of a\n" +
+                "page when looking at its layout.";
+        String actual = reader.readString();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(description = "Negative script for file data reader method",
+            expectedExceptions = {FileException.class})
+    public void testReadStringList2() throws FileException {
+        reader = new FileDataReader(NULL_FILE_PATH);
+        String expected = "";
+        String actual = reader.readString();
+        Assert.assertEquals(actual, expected);
+    }
+}
