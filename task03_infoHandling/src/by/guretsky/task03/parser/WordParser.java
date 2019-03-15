212928304fcd7bb564546bd0cb390e3e401edf2f
ilyaguretsky <borec023017@gmail.com>
2019-03-16 02:19:56 +0300

    Update parser

--- task03_infoHandling/src/by/guretsky/task03/parser/WordParser.java
+++ task03_infoHandling/src/by/guretsky/task03/parser/WordParser.java
@@ -4,6 +4,7 @@ import by.guretsky.task03.entity.Component;
 
 public class WordParser extends AbstractParser {
     private static final String PARSE_TO_WORD_REGEX = "";
+
     @Override
     public void parse(Component component) {
         parse(PARSE_TO_WORD_REGEX, component);
