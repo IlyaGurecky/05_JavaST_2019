e2d3dd108a3b2d140c3061ab7fd7b8c33cce89ed
ilyaguretsky <borec023017@gmail.com>
2019-03-19 17:06:35 +0300

    Update runner

--- task03_infoHandling/src/by/guretsky/task03/runner/Runner.java
+++ task03_infoHandling/src/by/guretsky/task03/runner/Runner.java
@@ -3,21 +3,21 @@ package by.guretsky.task03.runner;
 import by.guretsky.task03.entity.TextComponent;
 import by.guretsky.task03.entity.constant.TreeLevel;
 import by.guretsky.task03.exception.FileException;
-import by.guretsky.task03.exception.IncorrectArgumentException;
 import by.guretsky.task03.parser.*;
 import by.guretsky.task03.reader.FileDataReader;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Runner {
     private static final String PATH = "data" + File.separator + "info.txt";
 
-    public static void main(String[] args) throws FileException, IncorrectArgumentException {
+    public static void main(String[] args) throws FileException {
         FileDataReader reader = new FileDataReader(PATH);
         String string = reader.readString();
 
         TextComponent parsedText =
-                new TextComponent(string, TreeLevel.TEXT.getLevel());
+                new TextComponent(string, TreeLevel.TEXT);
 
         AbstractParser parser = new ParseStarter();
         AbstractParser paragraphParser = new ParagraphParser();
@@ -31,8 +31,7 @@ public class Runner {
         sentenceParser.linkWith(lexemeParser);
         lexemeParser.linkWith(lexemeComponentParser);
         lexemeComponentParser.linkWith(symbolParser);
-
-        parser.parse(parsedText);
-        parsedText.printComponent(1);
+        parser.parse(parsedText, string);
+        System.out.println(parsedText);
     }
 }
