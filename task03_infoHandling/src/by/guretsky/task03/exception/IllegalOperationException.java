6897cf60c5e2ab284ee071f6cbecf8c994b4ad08
ilyaguretsky <borec023017@gmail.com>
2019-03-21 01:06:47 +0300

    Add exception

---
+++ task03_infoHandling/src/by/guretsky/task03/exception/IllegalOperationException.java
@@ -0,0 +1,21 @@
+package by.guretsky.task03.exception;
+
+/**
+ * Signals about operation error.
+ */
+public class IllegalOperationException extends Exception {
+    /**
+     * Constructs a new exception.
+     */
+    public IllegalOperationException() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message detail message
+     */
+    public IllegalOperationException(String message) {
+        super(message);
+    }
+}
