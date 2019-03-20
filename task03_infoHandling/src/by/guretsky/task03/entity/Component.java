4facdb3ed82757cbc43129d9a848c92196c01d8f
ilyaguretsky <borec023017@gmail.com>
2019-03-21 01:05:25 +0300

    Update Component

--- task03_infoHandling/src/by/guretsky/task03/entity/Component.java
+++ task03_infoHandling/src/by/guretsky/task03/entity/Component.java
@@ -1,14 +1,16 @@
 package by.guretsky.task03.entity;
 
 import by.guretsky.task03.entity.constant.TreeLevel;
+import by.guretsky.task03.exception.IllegalOperationException;
 import by.guretsky.task03.exception.IncorrectArgumentException;
 
 public interface Component {
-    void add(final Component component);
+    void add(final Component component) throws IllegalOperationException;
 
-    void remove(final Component component);
+    void remove(final Component component) throws IllegalOperationException;
 
-    Component getChild(final int index) throws IncorrectArgumentException;
+    Component getChild(final int index) throws IncorrectArgumentException,
+            IllegalOperationException;
 
     TreeLevel getLevel();
 }
