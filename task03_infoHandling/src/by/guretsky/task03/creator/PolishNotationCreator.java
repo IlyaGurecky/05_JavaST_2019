e413f79d17f5914230fd7ee5e78a45bcac0cb9ab
ilyaguretsky <borec023017@gmail.com>
2019-03-22 15:35:30 +0300

    Add polish notation creator and test for this class

---
+++ task03_infoHandling/src/by/guretsky/task03/creator/PolishNotationCreator.java
@@ -0,0 +1,118 @@
+package by.guretsky.task03.creator;
+
+import by.guretsky.task03.exception.IncorrectArgumentException;
+import by.guretsky.task03.interpreter.operation.BinaryOperation;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PolishNotationCreator {
+    private static final String SPLIT_EXPRESSION_TO_NUMBERS_AND_FIGURES_REGEX =
+            "(?:>{3}|>{2}|<{2}|~|&|\\)|\\(|\\^|[0-9]+|\\|)";
+    private List<String> polishNotation = new ArrayList<>();
+    private Deque<BinaryOperation> operations = new ArrayDeque<>();
+
+    public List<String> createPolishNotation(final String expression) throws
+            IncorrectArgumentException {
+        if (expression == null || expression.isEmpty()) {
+            throw new IncorrectArgumentException("Incorrect expression");
+        }
+        List<String> brokenExpression = splitExpression(expression);
+        for (String element : brokenExpression) {
+            if (checkForNumber(element)) {
+                addElementToNotation(element);
+            } else {
+                if (isBracket(element)) {
+                    continue;
+                }
+                appendOperation(element);
+            }
+        }
+        addRemainingOperations();
+        return polishNotation;
+    }
+
+    private List<String> splitExpression(final String expression) {
+        Pattern pattern =
+                Pattern.compile(SPLIT_EXPRESSION_TO_NUMBERS_AND_FIGURES_REGEX);
+        Matcher matcher = pattern.matcher(expression);
+        List<String> expressionElements = new ArrayList<>();
+        while (matcher.find()) {
+            expressionElements.add(matcher.group(0));
+        }
+        return expressionElements;
+    }
+
+    private boolean checkForNumber(final String element) {
+        return element.matches("\\d+");
+    }
+
+    private void addElementToNotation(String element) {
+        polishNotation.add(element);
+    }
+
+    private BinaryOperation findOperation(String element)
+            throws IncorrectArgumentException {
+        switch (element) {
+            case "(":
+                return BinaryOperation.LEFT_BRACKET;
+            case ")":
+                return BinaryOperation.RIGHT_BRACKET;
+            case "~":
+                return BinaryOperation.NOT;
+            case "<<":
+                return BinaryOperation.LEFT_SHIFT;
+            case ">>":
+                return BinaryOperation.RIGHT_SHIFT;
+            case ">>>":
+                return BinaryOperation.UNSIGNED_RIGHT_SHIFT;
+            case "&":
+                return BinaryOperation.AND;
+            case "^":
+                return BinaryOperation.XOR;
+            case "|":
+                return BinaryOperation.OR;
+            default:
+                throw new IncorrectArgumentException("Operation doesn't exist");
+        }
+    }
+
+    private boolean isBracket(String element) {
+        if ("(".equals(element)) {
+            operations.add(BinaryOperation.LEFT_BRACKET);
+            return true;
+        } else if (")".equals(element)) {
+            while (!operations.peekLast()
+                    .equals(BinaryOperation.LEFT_BRACKET)) {
+                polishNotation.add(operations.pollLast().getOperator());
+            }
+            operations.pollLast();
+            return true;
+        }
+        return false;
+    }
+
+    private void appendOperation(final String element) throws
+            IncorrectArgumentException {
+        if (operations.isEmpty()) {
+            operations.add(findOperation(element));
+        } else {
+            BinaryOperation operation = findOperation(element);
+            if (operations.peekLast().getPriority()
+                    < operation.getPriority()) {
+                operations.add(operation);
+            } else if (operations.peekLast().getPriority()
+                    >= operation.getPriority()) {
+                addElementToNotation(operations.pollLast().getOperator());
+                operations.add(operation);
+            }
+        }
+    }
+
+    private void addRemainingOperations() {
+        while (!operations.isEmpty()) {
+            polishNotation.add(operations.pollLast().getOperator());
+        }
+    }
+}
---
+++ task03_infoHandling/src/test/by/guretsky/task03/creator/PolishNotationCreatorTest.java
@@ -0,0 +1,73 @@
+package test.by.guretsky.task03.creator;
+
+import by.guretsky.task03.creator.PolishNotationCreator;
+import by.guretsky.task03.exception.IncorrectArgumentException;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PolishNotationCreatorTest {
+    private final PolishNotationCreator creator = new PolishNotationCreator();
+
+    @DataProvider(name = "data_for_polish_notation_creator")
+    public Object[][] createCorrectData1() {
+        final List<String> expectedNotation1 =
+                new ArrayList<>(Arrays.asList("30", "3", ">>>"));
+        final List<String> expectedNotation2 =
+                new ArrayList<>(Arrays.asList("13", "2", "<<"));
+        final List<String> expectedNotation3 =
+                new ArrayList<>(Arrays.asList("5", "1", "2", "&", "3", "4",
+                        "25", "5", "^", "6", "47", "&", "|", "&", "3", "|", "|",
+                        "2", "|", "&", "1", "|", "|"));
+        final List<String> expectedNotation4 =
+                new ArrayList<>(Arrays.asList("71", "~", "2", "3", "&", "3",
+                        "2", "1", "2", ">>", "2", "|", "&", "2", "&", "|", "|",
+                        "10", "2", "&", "|", "&", "78", "|"));
+        final List<String> expectedNotation5 =
+                new ArrayList<>(Arrays.asList("111", "5", "^", "1", "2", "2",
+                        "5", "2", ">>", "71", "&", "|", "<<", "&", "|",
+                        "1200", "|"));
+        final List<String> expectedNotation6 =
+                new ArrayList<>(Arrays.asList("4", "5", "~", "|"));
+
+        return new Object[][]{
+                {"30>>>3", expectedNotation1},
+                {"13<<2", expectedNotation2},
+                {"5|(1&2&(3|(4&(25^5|6&47)|3)|2)|1)", expectedNotation3},
+                {"(~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78", expectedNotation4},
+                {"(111^5|1&2<<(2|5>>2&71))|1200", expectedNotation5},
+                {"4|~5", expectedNotation6}
+        };
+    }
+
+    @DataProvider(name = "negative_data_for_polish_notation_creator")
+    public Object[][] createCorrectData2() {
+        return new Object[][]{
+                {null, Arrays.asList("", "")},
+                {"", Arrays.asList("", "")},
+        };
+    }
+
+    @Test(description = "Positive exception for polish notation creator",
+            dataProvider = "data_for_polish_notation_creator")
+    public void testCreatePolishNotation(final String expression,
+                                         final List<String> expectedNotation)
+            throws IncorrectArgumentException {
+        List<String> actualNotation = creator.createPolishNotation(expression);
+        Assert.assertEquals(actualNotation, expectedNotation);
+    }
+
+    @Test(description = "Negative exception for polish notation creator",
+            dataProvider = "negative_data_for_polish_notation_creator",
+            expectedExceptions = {IncorrectArgumentException.class})
+    public void testCreatePolishNotation2(final String expression,
+                                          final List<String> expectedNotation)
+            throws IncorrectArgumentException {
+        List<String> actualNotation = creator.createPolishNotation(expression);
+        Assert.assertEquals(actualNotation, expectedNotation);
+    }
+}
