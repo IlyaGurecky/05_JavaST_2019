ef62ce6faf8f08051475560bd2c1fd66f1796ff7
ilyaguretsky <borec023017@gmail.com>
2019-03-23 14:09:12 +0300

    Add package-info files

---
+++ task03_infoHandling/src/by/guretsky/task03/action/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package consists of the classes, which sort text according to our rules.
+ */
+package by.guretsky.task03.action;
---
+++ task03_infoHandling/src/by/guretsky/task03/sort/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The package consists of text sort classes.
+ */
+package by.guretsky.task03.sort;
