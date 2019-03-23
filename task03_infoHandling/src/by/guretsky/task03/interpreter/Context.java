6e5f3f288ab5028538a77b2cfb14176db257f640
ilyaguretsky <borec023017@gmail.com>
2019-03-23 12:34:39 +0300

    Update interpreter

--- task03_infoHandling/src/by/guretsky/task03/interpreter/Context.java
+++ task03_infoHandling/src/by/guretsky/task03/interpreter/Context.java
@@ -3,13 +3,29 @@ package by.guretsky.task03.interpreter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * This class is used to store intermediate calculations and final decision.
+ */
 class Context {
+    /**
+     * List of calculations.
+     */
     private Deque<Integer> contextValues = new ArrayDeque<>();
 
+    /**
+     * This method is used for get first element from queue.
+     *
+     * @return integee number
+     */
     Integer poll() {
         return contextValues.pollFirst();
     }
 
+    /**
+     * This method is used for add number into the list.
+     *
+     * @param value value you need to add
+     */
     void push(final Integer value) {
         contextValues.push(value);
     }
--- task03_infoHandling/src/by/guretsky/task03/interpreter/Expression.java
+++ task03_infoHandling/src/by/guretsky/task03/interpreter/Expression.java
@@ -1,6 +1,14 @@
 package by.guretsky.task03.interpreter;
 
+/**
+ * Functional interface is used for Interpreter pattern.
+ */
 @FunctionalInterface
 public interface Expression {
+    /**
+     * Interpret method.
+     *
+     * @param context context object
+     */
     void interpret(Context context);
 }
