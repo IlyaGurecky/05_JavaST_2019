3ee6e97d84fb980a3939f1084eb3fcef0a68701d
ilyaguretsky <borec023017@gmail.com>
2019-03-24 19:18:23 +0300

    Add text tree creator

---
+++ task03_infoHandling/src/by/guretsky/task03/creator/TextTreeCreator.java
@@ -0,0 +1,38 @@
+package by.guretsky.task03.creator;
+
+import by.guretsky.task03.entity.Component;
+import by.guretsky.task03.entity.TextComponent;
+import by.guretsky.task03.entity.constant.TreeLevel;
+import by.guretsky.task03.parser.*;
+
+/**
+ * This class includes the method to create text components tree.
+ */
+public class TextTreeCreator {
+
+    /**
+     * Create text component tree.
+     *
+     * @param data string text
+     * @return text component
+     */
+    public Component createTextTree(final String data) {
+        Component text = new TextComponent(TreeLevel.TEXT);
+
+        AbstractParser parser = new ParseStarter();
+        AbstractParser paragraphParser = new ParagraphParser();
+        AbstractParser sentenceParser = new SentenceParser();
+        AbstractParser lexemeParser = new LexemeParser();
+        AbstractParser lexemeComponentParser = new LexemeComponentParser();
+        AbstractParser symbolParser = new SymbolParser();
+
+        parser.linkWith(paragraphParser);
+        paragraphParser.linkWith(sentenceParser);
+        sentenceParser.linkWith(lexemeParser);
+        lexemeParser.linkWith(lexemeComponentParser);
+        lexemeComponentParser.linkWith(symbolParser);
+        parser.parse(text, data);
+
+        return text;
+    }
+}
