3aced84e3232f8e41d6bd08afece447311bdd0bc
ilyaguretsky <borec023017@gmail.com>
2019-03-30 02:07:43 +0300

    Add exception

---
+++ Task04_XML_XSD_Web_Parsing/src/by/guretsky/task04/exception/IllegalTagNameException.java
@@ -0,0 +1,10 @@
+package by.guretsky.task04.exception;
+
+public class IllegalTagNameException extends Exception {
+    public IllegalTagNameException() {
+    }
+
+    public IllegalTagNameException(String message) {
+        super(message);
+    }
+}
