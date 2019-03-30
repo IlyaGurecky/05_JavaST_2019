66bff56500fb2d33d81c37f9a83033841bba693e
ilyaguretsky <borec023017@gmail.com>
2019-03-30 23:40:18 +0300

    Update Tariff Handler

--- Task04_XML_XSD_Web_Parsing/src/by/guretsky/task04/builder/TariffHandler.java
+++ Task04_XML_XSD_Web_Parsing/src/by/guretsky/task04/builder/TariffHandler.java
@@ -93,33 +93,6 @@ public class TariffHandler extends DefaultHandler {
         String s = new String(ch, start, length).trim();
         if (currentEnum != null) {
             switch (currentEnum.getValue()) {
-                case "name":
-                    current.setName(s);
-                    break;
-                case "operator":
-                    current.setOperator(s);
-                    break;
-                case "payroll":
-                    current.setPayroll(Double.parseDouble(s));
-                    break;
-                case "tariff-date":
-                    SimpleDateFormat format =
-                            new SimpleDateFormat("yyyy-MM-dd");
-                    try {
-                        current.setTariffDate(format.parse(s));
-                    } catch (ParseException e) {
-                        LOGGER.error("Date parse error");
-                    }
-                    break;
-                case "end-date":
-                    try {
-                        current.setEndDate(
-                                new SimpleDateFormat("yyyy-MM-dd")
-                                        .parse(s));
-                    } catch (ParseException e) {
-                        LOGGER.error("Date parse error");
-                    }
-                    break;
                 case "connection-price":
                     current.getParameters()
                             .setConnectionPrice(Double.parseDouble(s));
@@ -129,57 +102,138 @@ public class TariffHandler extends DefaultHandler {
                             .setBlockingWithDebt(Boolean.parseBoolean(s));
                     break;
                 case "outside":
-                    if ("Calls".equals(current.getClass().getSimpleName())) {
-                        ((Calls) current).getCallPrices()
-                                .setOutside(Double.parseDouble(s));
-                    } else {
-                        ((CallsAndInternet) current).getCallPrices()
-                                .setOutside(Double.parseDouble(s));
-                    }
+                    definePricesProperties(
+                            TariffsEnum.OUTSIDE.getValue(), s);
                     break;
                 case "inside":
-                    if ("Calls".equals(current.getClass().getSimpleName())) {
-                        ((Calls) current).getCallPrices()
-                                .setInside(Double.parseDouble(s));
-                    } else {
-                        ((CallsAndInternet) current).getCallPrices()
-                                .setInside(Double.parseDouble(s));
-                    }
+                    definePricesProperties(
+                            TariffsEnum.INSIDE.getValue(), s);
                     break;
                 case "landline":
-                    if ("Calls".equals(current.getClass().getSimpleName())) {
-                        ((Calls) current).getCallPrices()
-                                .setLandLine(Double.parseDouble(s));
-                    } else {
-                        ((CallsAndInternet) current).getCallPrices()
-                                .setLandLine(Double.parseDouble(s));
-                    }
+                    definePricesProperties(
+                            TariffsEnum.LANDLINE.getValue(), s);
                     break;
                 case "SMS-price":
-                    if ("Calls".equals(current.getClass().getSimpleName())) {
-                        ((Calls) current).setSMSPrice(Double.parseDouble(s));
-                    } else {
-                        ((CallsAndInternet) current)
-                                .setSMSPrice(Double.parseDouble(s));
-                    }
+                    definePricesProperties(TariffsEnum.SMS_PRICE.getValue(), s);
                     break;
                 case "free-mb":
-                    if ("Internet".equals(current.getClass().getSimpleName())) {
-                        ((Internet) current).setFreeMb(Integer.parseInt(s));
-                    } else {
-                        ((CallsAndInternet) current)
-                                .setFreeMb(Integer.parseInt(s));
-                    }
+                    defineInternetProperties(TariffsEnum.FREE_MB.getValue(), s);
                     break;
                 case "speed-limit":
-                    if ("Internet".equals(current.getClass().getSimpleName())) {
-                        ((Internet) current).setSpeedLimit(Integer.parseInt(s));
-                    } else {
-                        ((CallsAndInternet) current)
-                                .setSpeedLimit(Integer.parseInt(s));
+                    defineInternetProperties(
+                            TariffsEnum.SPEED_LIMIT.getValue(), s);
+                    break;
+                default:
+                    try {
+                        defineCommonProperties(s);
+                    } catch (IllegalTagNameException e) {
+                        LOGGER.error("Get common properties error", e);
                     }
+                    break;
             }
             currentEnum = null;
+        }
+    }
+
+    private void defineCommonProperties(String s) throws
+            IllegalTagNameException {
+        switch (currentEnum.getValue()) {
+            case "name":
+                current.setName(s);
+                break;
+            case "operator":
+                current.setOperator(s);
+                break;
+            case "payroll":
+                current.setPayroll(Double.parseDouble(s));
+                break;
+            case "tariff-date":
+                SimpleDateFormat format =
+                        new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    current.setTariffDate(format.parse(s));
+                } catch (ParseException e) {
+                    LOGGER.error("Date parse error");
+                }
+                break;
+            case "end-date":
+                try {
+                    current.setEndDate(
+                            new SimpleDateFormat("yyyy-MM-dd")
+                                    .parse(s));
+                } catch (ParseException e) {
+                    LOGGER.error("Date parse error");
+                }
+                break;
+            default:
+                throw new IllegalTagNameException("Unknown tag");
+        }
+
+    }
+
+    private void definePricesProperties(String property, String s) {
+        switch (property) {
+            case "outside":
+                if (current instanceof Calls) {
+                    ((Calls) current).getCallPrices()
+                            .setOutside(Double.parseDouble(s));
+                } else {
+                    ((CallsAndInternet) current).getCallPrices()
+                            .setOutside(Double.parseDouble(s));
+                }
+                break;
+            case "inside":
+                if (current instanceof Calls) {
+                    ((Calls) current).getCallPrices()
+                            .setInside(Double.parseDouble(s));
+                } else {
+                    ((CallsAndInternet) current).getCallPrices()
+                            .setInside(Double.parseDouble(s));
+                }
+                break;
+            case "landline":
+                if (current instanceof Calls) {
+                    ((Calls) current).getCallPrices()
+                            .setLandLine(Double.parseDouble(s));
+                } else {
+                    ((CallsAndInternet) current).getCallPrices()
+                            .setLandLine(Double.parseDouble(s));
+                }
+                break;
+            case "SMS-price":
+                if (current instanceof Calls) {
+                    ((Calls) current).setSMSPrice(Double.parseDouble(s));
+                } else {
+                    ((CallsAndInternet) current)
+                            .setSMSPrice(Double.parseDouble(s));
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void defineInternetProperties(String property, String s) {
+        switch (property) {
+            case "free-mb":
+                if (current instanceof Internet) {
+                    ((Internet) current).setFreeMb(Integer.parseInt(s));
+                } else {
+                    ((CallsAndInternet) current)
+                            .setFreeMb(Integer.parseInt(s));
+                }
+                break;
+            case "speed-limit":
+                if (current instanceof Internet) {
+                    ((Internet) current).setSpeedLimit(Integer.parseInt(s));
+                } else {
+                    ((CallsAndInternet) current)
+                            .setSpeedLimit(Integer.parseInt(s));
+                }
+                break;
+            default:
+                break;
         }
     }
 }
