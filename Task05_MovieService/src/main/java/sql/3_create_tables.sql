8b4c964b132faf599c3e7a57e1fcc85aaaecfb48
ilyaguretsky <borec023017@gmail.com>
2019-04-26 17:33:18 +0300

    Update create tables script

--- Task05_MovieService/src/main/java/sql/3_create_tables.sql
+++ Task05_MovieService/src/main/java/sql/3_create_tables.sql
@@ -3,8 +3,8 @@ USE `movie_service_db`;
 CREATE TABLE `users`
 (
   `id`       INTEGER      NOT NULL AUTO_INCREMENT,
-  `login`    VARCHAR(255) NOT NULL UNIQUE,
-  `password` CHAR(32)     NOT NULL,
+  `login`    VARCHAR(32)  NOT NULL UNIQUE,
+  `password` VARCHAR(100) NOT NULL,
   /*
    * 0 - администратор (Role.ADMIN)
    * 1 - редактор (Role.EDITOR)
@@ -12,63 +12,60 @@ CREATE TABLE `users`
    */
   `role`     TINYINT      NOT NULL CHECK (`role` IN (0, 1, 2)),
   PRIMARY KEY (`id`)
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
+) DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE `countries_catalog`
+(
+  `id`   INTEGER      NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `user_info`
 (
-  `id`      INTEGER NOT NULL AUTO_INCREMENT,
-  `email`   VARCHAR(255) UNIQUE,
-  `sex`     VARCHAR(6),
-  `age`     TINYINT,
-  `country` VARCHAR(100),
-  `user_id` INTEGER NOT NULL UNIQUE,
+  `id`         INTEGER NOT NULL AUTO_INCREMENT,
+  `email`      VARCHAR(50) UNIQUE,
+  `sex`        CHAR(1) CHECK (`sex` IN ('м', 'ж')),
+  `birth_date` DATE,
+  `country_id` INTEGER,
+  `user_id`    INTEGER NOT NULL UNIQUE,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`)
     REFERENCES `users` (`id`)
     ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (`country_id`)
+    REFERENCES `countries_catalog` (`id`)
+    ON UPDATE CASCADE
     ON DELETE CASCADE
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
+) DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE `categories_catalog`
+(
+  `id`   INTEGER      NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `films`
 (
   `id`           INTEGER      NOT NULL AUTO_INCREMENT,
   `name`         VARCHAR(255) NOT NULL,
   `premier_date` DATE         NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
-
-CREATE TABLE `category`
-(
-  `id`   INTEGER      NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
-
-CREATE TABLE `film_info`
-(
-  `id`          INTEGER NOT NULL AUTO_INCREMENT,
-  `film_id`     INTEGER NOT NULL UNIQUE,
-  `country`     VARCHAR(50),
-  `producer`    VARCHAR(100),
-  `actors`      TEXT,
-  `category_id` INTEGER NOT NULL,
-  `image`       VARCHAR(100),
-  `description` TEXT,
+  `country_id`   INTEGER,
+  `category_id`  INTEGER,
+  `image_path`   VARCHAR(100),
+  `description`  TEXT,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`film_id`)
-    REFERENCES `films` (`id`)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
   FOREIGN KEY (`category_id`)
-    REFERENCES `category` (`id`)
+    REFERENCES `categories_catalog` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+  FOREIGN KEY (`country_id`)
+    REFERENCES `countries_catalog` (`id`)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
+) DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `see_later`
 (
@@ -84,9 +81,9 @@ CREATE TABLE `see_later`
   FOREIGN KEY (`film_id`)
     REFERENCES `films` (`id`)
     ON UPDATE CASCADE
-    ON DELETE CASCADE
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
+    ON DELETE CASCADE,
+  UNIQUE (user_id, film_id)
+) DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `watched_films`
 (
@@ -103,5 +100,4 @@ CREATE TABLE `watched_films`
     REFERENCES `films` (`id`)
     ON UPDATE CASCADE
     ON DELETE CASCADE
-) ENGINE = INNODB
-  DEFAULT CHARACTER SET utf8;
+) DEFAULT CHARACTER SET utf8;
