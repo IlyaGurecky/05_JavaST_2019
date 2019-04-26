63e3b0b20f5a9e8810ce58e0bdd7654b88f8ca16
ilyaguretsky <borec023017@gmail.com>
2019-04-26 19:46:35 +0300

    Update sql scripts

--- Task05_MovieService/src/main/java/sql/2_create_db.sql
+++ Task05_MovieService/src/main/java/sql/2_create_db.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE `movie_service_db` DEFAULT CHARACTER SET utf8;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `movie_service_db`.* TO 'movie_service_user'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `movie_service_db`.*
+  TO 'movie_service_user'@'localhost';
