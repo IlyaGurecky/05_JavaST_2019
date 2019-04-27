aaf9d1a21abe4dc5e6c306d191c119f3555cb6a6
ilyaguretsky <borec023017@gmail.com>
2019-04-27 12:06:00 +0300

    Update db create script

--- Task05_MovieService/src/main/java/sql/2_create_db.sql
+++ Task05_MovieService/src/main/java/sql/2_create_db.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE `movie_service_db` DEFAULT CHARACTER SET utf8;
 
+CREATE USER IF NOT EXISTS 'movie_service_user'@'localhost' IDENTIFIED BY 'password';
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON `movie_service_db`.*
   TO 'movie_service_user'@'localhost';
