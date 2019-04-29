ea39641493c691b0f2a2a825b8eeb322b950ddf3
ilyaguretsky <borec023017@gmail.com>
2019-04-29 20:35:53 +0300

    Update role enum

--- Task05_MovieService/src/main/java/by/guretsky/info_system/entity/role/Role.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/entity/role/Role.java
@@ -1,17 +1,23 @@
 package by.guretsky.info_system.entity.role;
 
 public enum Role {
-    ADMIN("admin"),
-    EDITOR("editor"),
-    USER("user");
+    ADMIN("admin", 0),
+    EDITOR("editor", 1),
+    USER("user", 2);
 
     private String value;
+    private int id;
 
-    Role(final String role) {
+    Role(final String role, int identity) {
         value = role;
+        id = identity;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public int getId() {
+        return id;
     }
 }
