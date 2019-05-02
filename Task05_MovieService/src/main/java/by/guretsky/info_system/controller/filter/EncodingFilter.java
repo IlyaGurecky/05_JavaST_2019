f7e0ed3526b4d5c5d73074975acc91ac1ef1de27
ilyaguretsky <borec023017@gmail.com>
2019-05-02 23:54:42 +0300

    Add encoding filter

---
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/controller/filter/EncodingFilter.java
@@ -0,0 +1,29 @@
+package by.guretsky.info_system.controller.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest,
+                         final ServletResponse servletResponse,
+                         final FilterChain filterChain) throws IOException,
+            ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
