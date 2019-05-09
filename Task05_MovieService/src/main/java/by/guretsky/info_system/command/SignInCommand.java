faff2d13220b102b4aaf2a29a6a0ea6a2ae3be0e
ilyaguretsky <borec023017@gmail.com>
2019-05-09 20:36:32 +0300

    Add sign in action

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/SignInCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/SignInCommand.java
@@ -3,13 +3,12 @@ package by.guretsky.info_system.command;
 import by.guretsky.info_system.entity.User;
 import by.guretsky.info_system.exception.CustomException;
 import by.guretsky.info_system.service.UserService;
-import by.guretsky.info_system.service.impl.UserServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-public class AuthorizationCommand implements ActionCommand {
-    private static final String FORWARD_PAGE = "/index.jsp";
+public class SignInCommand extends ActionCommand {
+    private static final String FORWARD_PAGE = "/home";
     private static final String LOGIN_PARAM = "login";
     private static final String PASSWORD_PARAM = "password";
 
@@ -18,14 +17,14 @@ public class AuthorizationCommand implements ActionCommand {
         String login = request.getParameter(LOGIN_PARAM);
         String password = request.getParameter(PASSWORD_PARAM);
         if (login != null && password != null) {
-            UserService service = new UserServiceImpl();
+            UserService service = factory.createService(UserService.class);
             User user = service.findByLoginAndPassword(login, password);
             if (user != null) {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
                 return FORWARD_PAGE;
             } else {
-                request.setAttribute("signInErrorMessage",
+                request.setAttribute("error",
                         "Login or Password is incorrect");
             }
         }
