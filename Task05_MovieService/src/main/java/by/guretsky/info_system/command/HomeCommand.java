7fbd6a2300a7703cfc460e3fb57e119e52587543
ilyaguretsky <borec023017@gmail.com>
2019-05-09 20:39:48 +0300

    Add home action

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/HomeCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/HomeCommand.java
@@ -2,7 +2,7 @@ package by.guretsky.info_system.command;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class HomeCommand implements ActionCommand {
+public class HomeCommand extends ActionCommand {
     @Override
     public String execute(HttpServletRequest request) {
         return null;
