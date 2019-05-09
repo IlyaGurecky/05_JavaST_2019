767ec93f5fed2738937a2d4aec823956b33393ec
ilyaguretsky <borec023017@gmail.com>
2019-05-09 20:39:13 +0300

    Update action abstract class

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/ActionCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/ActionCommand.java
@@ -1,10 +1,26 @@
 package by.guretsky.info_system.command;
 
 import by.guretsky.info_system.exception.CustomException;
+import by.guretsky.info_system.service.ServiceFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
-public interface ActionCommand {
+public abstract class ActionCommand {
+    protected ServiceFactory factory;
+    private String name;
 
-    String execute(HttpServletRequest request) throws CustomException;
+    abstract String execute(final HttpServletRequest request) throws
+            CustomException;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String actionName) {
+        this.name = actionName;
+    }
+
+    public void setFactory(final ServiceFactory serviceFactory) {
+        this.factory = serviceFactory;
+    }
 }
