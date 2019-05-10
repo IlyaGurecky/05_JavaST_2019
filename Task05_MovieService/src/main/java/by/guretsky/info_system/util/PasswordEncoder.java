08626706171bb0d54d27e4631652819b5736f66b
ilyaguretsky <borec023017@gmail.com>
2019-05-10 18:21:51 +0300

    Add password encoder

---
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/util/PasswordEncoder.java
@@ -0,0 +1,18 @@
+package by.guretsky.info_system.util;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class PasswordEncoder {
+
+    private PasswordEncoder() {
+    }
+
+    public static boolean checkPassword(final String password, final String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
+
+    public static String hashPassword(final String password) {
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(password, salt);
+    }
+}
