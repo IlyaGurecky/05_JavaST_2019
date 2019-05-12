de3596bb8bf03b1dc6a4ca182404efaa0d755f17
ilyaguretsky <borec023017@gmail.com>
2019-05-12 23:23:32 +0300

    Update css files

--- Task05_MovieService/src/main/java/by/guretsky/info_system/runner/Runner.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/runner/Runner.java
@@ -1,11 +1,13 @@
 package by.guretsky.info_system.runner;
 
 import by.guretsky.info_system.entity.role.Role;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import java.sql.SQLException;
 
 public class Runner {
     public static void main(String[] args) throws SQLException {
+
     }
 }
 
