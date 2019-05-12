2da653e5ab0f8bfe953cebde1fc65b8f048f8c5d
ilyaguretsky <borec023017@gmail.com>
2019-05-12 23:22:37 +0300

    Update action enum

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
@@ -1,22 +1,26 @@
 package by.guretsky.info_system.command.constant;
 
-import by.guretsky.info_system.command.*;
+import by.guretsky.info_system.command.Command;
+import by.guretsky.info_system.command.LogoutCommand;
+import by.guretsky.info_system.command.SignInCommand;
+import by.guretsky.info_system.command.SignUpCommand;
+import by.guretsky.info_system.command.admin.UserDeleteCommand;
 
 public enum ActionEnum {
-    SIGN_IN(new SignInCommand(), "/signin"),
-    SIGN_UP(new SignUpCommand(), "/signup"),
-    HOME(new HomeCommand(), "/home"),
-    LOGOUT(new LogoutCommand(), "/logout");
+    SIGN_IN(new SignInCommand(), "signin"),
+    SIGN_UP(new SignUpCommand(), "signup"),
+    LOGOUT(new LogoutCommand(), "signout"),
+    DELETE_USER(new UserDeleteCommand(), "deleteUser");
 
-    private ActionCommand command;
+    private Command command;
     private String actionName;
 
-    ActionEnum(ActionCommand actionCommand, String name) {
-        command = actionCommand;
+    ActionEnum(Command command, String name) {
+        this.command = command;
         actionName = name;
     }
 
-    public ActionCommand getCommand() {
+    public Command getCommand() {
         return command;
     }
 
