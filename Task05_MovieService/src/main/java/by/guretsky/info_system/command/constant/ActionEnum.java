d69e8c28b0e739b27414be14c2a7eab0a7a7833d
ilyaguretsky <borec023017@gmail.com>
2019-05-14 22:39:13 +0300

    Add some new actions

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
@@ -4,13 +4,17 @@ import by.guretsky.info_system.command.Command;
 import by.guretsky.info_system.command.LogoutCommand;
 import by.guretsky.info_system.command.SignInCommand;
 import by.guretsky.info_system.command.SignUpCommand;
+import by.guretsky.info_system.command.admin.UserAddCommand;
 import by.guretsky.info_system.command.admin.UserDeleteCommand;
+import by.guretsky.info_system.command.user.AddToSeeLaterCommand;
 
 public enum ActionEnum {
     SIGN_IN(new SignInCommand(), "signin"),
     SIGN_UP(new SignUpCommand(), "signup"),
     LOGOUT(new LogoutCommand(), "signout"),
-    DELETE_USER(new UserDeleteCommand(), "deleteUser");
+    DELETE_USER(new UserDeleteCommand(), "deleteUser"),
+    ADD_USER(new UserAddCommand(), "addUser"),
+    ADD_TO_SEE_LATER(new AddToSeeLaterCommand(), "addToSeeLater");
 
     private Command command;
     private String actionName;
