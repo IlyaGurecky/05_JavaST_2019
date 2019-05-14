1d8b63ead307d627479e25f5e6599c1668a0ab1a
ilyaguretsky <borec023017@gmail.com>
2019-05-14 22:38:28 +0300

    Update user delete command(Add .trim())

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/UserDeleteCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/UserDeleteCommand.java
@@ -1,6 +1,7 @@
 package by.guretsky.info_system.command.admin;
 
 import by.guretsky.info_system.command.Command;
+import by.guretsky.info_system.entity.role.Role;
 import by.guretsky.info_system.exception.CustomException;
 import by.guretsky.info_system.page.JspPage;
 import by.guretsky.info_system.page.PageEnum;
@@ -14,8 +15,19 @@ public class UserDeleteCommand extends Command {
     public JspPage execute(HttpServletRequest request) throws CustomException {
         UserService service = factory.createService(UserService.class);
         String id = request.getParameter("id");
-        Integer userId = Integer.parseInt(id);
-        service.delete(userId);
-        return PageManager.createPage(PageEnum.USERS);
+        String login = request.getParameter("login");
+        if (login != null && !login.isEmpty()
+                && !service.findByLogin(login.trim()).getRole().equals(Role.ADMIN)) {
+
+            //TODO: Error message
+            service.deleteByLogin(login.trim());
+
+
+            return PageManager.createPage(PageEnum.USERS);
+        } else {
+            Integer userId = Integer.parseInt(id);
+            service.delete(userId);
+            return PageManager.createPage(PageEnum.USERS);
+        }
     }
 }
