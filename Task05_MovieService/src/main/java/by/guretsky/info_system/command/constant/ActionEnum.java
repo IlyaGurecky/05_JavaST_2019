f3a0f40d16181abdc017bfabc9fef9ac61bf8dfc
ilyaguretsky <borec023017@gmail.com>
2019-05-18 16:12:14 +0300

    Update action enum

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/constant/ActionEnum.java
@@ -4,10 +4,14 @@ import by.guretsky.info_system.command.Command;
 import by.guretsky.info_system.command.LogoutCommand;
 import by.guretsky.info_system.command.SignInCommand;
 import by.guretsky.info_system.command.SignUpCommand;
+import by.guretsky.info_system.command.admin.FilmAddCommand;
 import by.guretsky.info_system.command.admin.FilmDeleteCommand;
 import by.guretsky.info_system.command.admin.UserAddCommand;
 import by.guretsky.info_system.command.admin.UserDeleteCommand;
 import by.guretsky.info_system.command.user.AddToSeeLaterCommand;
+import by.guretsky.info_system.command.user.ChangePasswordCommand;
+import by.guretsky.info_system.command.user.EditProfileCommand;
+import by.guretsky.info_system.command.user.WatchFilmCommand;
 
 public enum ActionEnum {
     SIGN_IN(new SignInCommand(), "signin"),
@@ -16,7 +20,11 @@ public enum ActionEnum {
     DELETE_USER(new UserDeleteCommand(), "deleteUser"),
     ADD_USER(new UserAddCommand(), "addUser"),
     ADD_TO_SEE_LATER(new AddToSeeLaterCommand(), "addToSeeLater"),
-    DELETE_FILM(new FilmDeleteCommand(), "deleteFilm");
+    DELETE_FILM(new FilmDeleteCommand(), "deleteFilm"),
+    CHANGE_PASS(new ChangePasswordCommand(), "changePass"),
+    EDIT_PROFILE(new EditProfileCommand(), "editProfile"),
+    ADD_FILM(new FilmAddCommand(), "addFilm"),
+    WATCH_FILM(new WatchFilmCommand(), "watchFilm");
 
     private Command command;
     private String actionName;
