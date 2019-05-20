b27d4fb5305e332aebdda1998ecc3e2a76c2de41
ilyaguretsky <borec023017@gmail.com>
2019-05-20 23:41:50 +0300

    Add delete from see later action

---
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/user/DeleteFromSeeLaterCommand.java
@@ -0,0 +1,25 @@
+package by.guretsky.info_system.command.user;
+
+import by.guretsky.info_system.command.Command;
+import by.guretsky.info_system.entity.User;
+import by.guretsky.info_system.exception.CustomException;
+import by.guretsky.info_system.page.JspPage;
+import by.guretsky.info_system.page.PageEnum;
+import by.guretsky.info_system.page.PageManager;
+import by.guretsky.info_system.service.SeeLaterService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class DeleteFromSeeLaterCommand extends Command {
+    @Override
+    public JspPage execute(HttpServletRequest request) throws CustomException {
+        Integer filmId = Integer.parseInt(request.getParameter("id"));
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute("user");
+        Integer userId = user.getId();
+        SeeLaterService service = factory.createService(SeeLaterService.class);
+        service.deleteByUserAndFilmId(userId, filmId);
+        return PageManager.createPage(PageEnum.SEE_LATER);
+    }
+}
