9abdb7a2c2e74036f9c95ac3fdd86cd6f5061332
ilyaguretsky <borec023017@gmail.com>
2019-05-20 23:41:14 +0300

    Add the ability to upload film image

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/FilmAddCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/FilmAddCommand.java
@@ -10,13 +10,21 @@ import by.guretsky.info_system.service.FilmService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class FilmAddCommand extends Command {
     private static final Logger LOGGER =
             LogManager.getLogger(FilmAddCommand.class);
+    private static final String SPLIT_DIR_REGEX = "/";
 
     @Override
     public JspPage execute(HttpServletRequest request) throws CustomException {
@@ -24,10 +32,35 @@ public class FilmAddCommand extends Command {
         String premierDate = request.getParameter("premier_date");
         String country = request.getParameter("country");
         String category = request.getParameter("category");
-        String imageName = request.getParameter("imageName");
 
         if (isCorrectData(name, premierDate)) {
             Film film = new Film();
+            String imageName = null;
+            try {
+                Part part = request.getPart("filmImage");
+                InputStream fileContent = part.getInputStream();
+                imageName =
+                        Paths.get(part.getSubmittedFileName()).getFileName()
+                                .toString();
+                String descPath = request.getServletContext()
+                        .getRealPath("");
+                String[] dirs = descPath.split(SPLIT_DIR_REGEX);
+                StringBuilder buffer = new StringBuilder();
+                for (int i = 0; i < dirs.length - 2; i++) {
+                    buffer.append(dirs[i]);
+                    buffer.append("/");
+                }
+                String pathToSave = buffer + "src/main/webapp/img/films/"
+                        + imageName;
+                OutputStream outputStream = new FileOutputStream(pathToSave);
+                fileContent.transferTo(outputStream);
+            } catch (IOException e) {
+                LOGGER.error("File path is incorrect", e);
+            } catch (ServletException e) {
+                LOGGER.error("Servlet exc", e);
+            }
+
+            film.setImageName(imageName);
             film.setName(name);
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
             try {
@@ -37,9 +70,6 @@ public class FilmAddCommand extends Command {
             }
             film.setCategory(category);
             film.setCountry(country);
-
-            film.setImageName(imageName);
-
             FilmService service = factory.createService(FilmService.class);
             if (service.create(film) != 0) {
                 return PageManager.createPage(PageEnum.FILMS);
