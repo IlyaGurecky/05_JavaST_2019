0d05d080c3fd94b3015b8d8e9aa7bdf414e44ced
ilyaguretsky <borec023017@gmail.com>
2019-05-20 23:42:31 +0300

    Add multipart config annotation

--- Task05_MovieService/src/main/java/by/guretsky/info_system/controller/MainController.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/controller/MainController.java
@@ -12,11 +12,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@MultipartConfig
 public class MainController extends HttpServlet {
     private static final Logger LOGGER =
             LogManager.getLogger(MainController.class);
