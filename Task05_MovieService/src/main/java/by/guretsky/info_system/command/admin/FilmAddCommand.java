daa09ecb545c6fbdfb0cac9087f40b6ea86b5a9b
ilyaguretsky <borec023017@gmail.com>
2019-05-21 18:07:50 +0300

    Update file upload

--- Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/FilmAddCommand.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/command/admin/FilmAddCommand.java
@@ -12,19 +12,13 @@ import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.Part;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class FilmAddCommand extends Command {
     private static final Logger LOGGER =
             LogManager.getLogger(FilmAddCommand.class);
-    private static final String SPLIT_DIR_REGEX = "/";
 
     @Override
     public JspPage execute(HttpServletRequest request) throws CustomException {
@@ -34,32 +28,16 @@ public class FilmAddCommand extends Command {
         String category = request.getParameter("category");
 
         if (isCorrectData(name, premierDate)) {
-            Film film = new Film();
             String imageName = null;
             try {
-                Part part = request.getPart("filmImage");
-                InputStream fileContent = part.getInputStream();
-                imageName =
-                        Paths.get(part.getSubmittedFileName()).getFileName()
-                                .toString();
-                String descPath = request.getServletContext()
-                        .getRealPath("");
-                String[] dirs = descPath.split(SPLIT_DIR_REGEX);
-                StringBuilder buffer = new StringBuilder();
-                for (int i = 0; i < dirs.length - 2; i++) {
-                    buffer.append(dirs[i]);
-                    buffer.append("/");
-                }
-                String pathToSave = buffer + "src/main/webapp/img/films/"
-                        + imageName;
-                OutputStream outputStream = new FileOutputStream(pathToSave);
-                fileContent.transferTo(outputStream);
+                imageName = uploadImage(request);
             } catch (IOException e) {
                 LOGGER.error("File path is incorrect", e);
             } catch (ServletException e) {
                 LOGGER.error("Servlet exc", e);
             }
 
+            Film film = new Film();
             film.setImageName(imageName);
             film.setName(name);
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
