818859a05a996d90eff60f221fab9a18877d0dbb
ilyaguretsky <borec023017@gmail.com>
2019-05-23 02:15:37 +0300

    Update see later dao

--- Task05_MovieService/src/main/java/by/guretsky/info_system/dao/SeeLaterDao.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/dao/SeeLaterDao.java
@@ -5,7 +5,13 @@ import by.guretsky.info_system.entity.SeeLater;
 import java.util.List;
 
 public interface SeeLaterDao extends Dao<SeeLater> {
-    List<SeeLater> readAllByUserId(final Integer id);
+    List<SeeLater> readAllByUserId(final Integer id,
+                                   final int pageNum,
+                                   final int amountPerPage);
+
+    List<SeeLater> readAllByUserId(final Integer userId);
 
     boolean deleteByUserAndFilmId(final Integer userId, final Integer filmId);
+
+    Integer countFilms(final int userId);
 }
