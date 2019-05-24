f952db53fae0004e7b87ce52ee3bdb34d63bac53
ilyaguretsky <borec023017@gmail.com>
2019-05-24 22:43:30 +0300

    Add login length check

--- Task05_MovieService/src/main/java/by/guretsky/info_system/validator/UserValidator.java
+++ Task05_MovieService/src/main/java/by/guretsky/info_system/validator/UserValidator.java
@@ -5,10 +5,11 @@ import by.guretsky.info_system.entity.User;
 public class UserValidator implements Validator<User> {
     private static final String EMAIL_REGEX
             = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$)";
-
+    private static final int MAX_LOGIN_LENGTH = 32;
 
     public boolean validate(final User user) {
-        return user.getEmail().matches(EMAIL_REGEX);
+        return user.getEmail().matches(EMAIL_REGEX)
+                && user.getLogin().length() <= MAX_LOGIN_LENGTH;
     }
 
     public boolean isCorrectEmail(final String email) {
